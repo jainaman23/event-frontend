73261c9f23dc9e8974a47d92d0acfbcabe19b3cb
Aman Jain <aman.jain@airtel.com>
2022-12-18 23:39:30 +0530

    added static files

---
+++ mhsosa/js/custom.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $(".navbar-toggler").click(function(){
+      $("body").addClass("menu_Active");
+    });
+    $(".navbarNav_cross").click(function(){
+      $("body").removeClass("menu_Active");
+    });
+    $('.st_1_itm_cn').matchHeight({byRow: true,});
+    $('.st_1_in.owl-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        autoHeight: true,
+        nav:false,
+        dots:true,   
+        autoplay:true,         
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    });
+});
