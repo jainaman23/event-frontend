9a5e5e2d814fdc8f4c75dd3dca1460e8cf9a9305
Aman Jain <aman.jain2@go-mmt.com>
2024-04-12 16:06:09 +0530

    mhsosa-event ticket added

--- pages/_document.js
+++ pages/_document.js
@@ -14,10 +14,12 @@ export default class MyDocument extends Document {
           <link
             href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap"
             rel="stylesheet"
+            crossOrigin="anonymous"
           ></link>
           <link
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500&display=swap"
             rel="stylesheet"
+            crossOrigin="anonymous"
           />
         </Head>
         <body>
--- src/components/molecules/EntryPass/index.js
+++ src/components/molecules/EntryPass/index.js
@@ -4,28 +4,28 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
 import QRCode from 'react-qr-code';
-import Item from '@components/atoms/GridItem';
 
 export default function EntryPass({ registrationId, registerName }) {
   return (
-    <Box component={Paper} sx={{ p: 2, border: '5px solid #2b81be' }} id="QRCode">
-      <Item>
-        <NextImage src="https://mhsosa.in/img/logo.png" width={108} height={106} alt="" />
-      </Item>
-      <Box sx={{ m: 2 }}>
-        <QRCode title="Entry Pass" value={registrationId} />
-      </Box>
-      <Item>
-        <Typography variant="h6" sx={{ mb: 1, fontWeight: 'bold' }}>
+    <Box sx={{ position: 'relative' }} id="QRCode">
+      <NextImage src="/mhsosa-ticket.jpg" width={400} height={700} alt="" />
+      <Box
+        sx={{
+          position: 'absolute',
+          bottom: 66,
+          display: 'flex',
+          flexDirection: 'column',
+          width: '100%',
+          alignItems: 'center',
+        }}
+      >
+        <Box component={Paper} sx={{ p: 1 }}>
+          <QRCode title="Entry Pass" value={registrationId} size={180} />
+        </Box>
+        <Typography variant="h6" sx={{ mt: 2, fontWeight: 'bold', color: '#fff', fontSize: 24 }}>
           {registerName}
         </Typography>
-        <Typography variant="h6" sx={{ fontWeight: 'bold' }}>
-          2nd Alumni Meet
-        </Typography>
-        <Typography sx={{ fontWeight: 500 }}>Sunday</Typography>
-        <Typography sx={{ fontWeight: 500 }}>5:00 PM Onwards</Typography>
-        <Typography sx={{ fontWeight: 500 }}>MHS School, Tilak Nagar, Jaipur</Typography>
-      </Item>
+      </Box>
     </Box>
   );
 }
